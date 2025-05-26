9906948361590bcc112dd6decdd240869e053855
Макс Титов <hhit934@gmail.com>
2025-05-26 23:58:18 +0300

    fix ip

--- LANFile/App.axaml.cs
+++ LANFile/App.axaml.cs
@@ -3,8 +3,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using Android.App;
-using Android.OS;
-using Avalonia;
+using Android.Net.Wifi;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
@@ -12,28 +11,51 @@ using LANFile.ViewModels;
 using LANFile.Views;
 using Application = Avalonia.Application;
 
+ 
+
 namespace LANFile;
 
 public class App : Application
 {
     public static Random R = new Random();
-    public static string NameApplication { get; set; } = $"{Guid.NewGuid().ToString().Substring(0,9)}";
+    public static string NameApplication { get; set; } = $"{Guid.NewGuid().ToString().Substring(0, 9)}";
+
     public static string Platform { get; set; } =
         $"{(OperatingSystem.IsAndroid() ? "Android" : "Windows")}";
-    
+
     public static string GetLocalIPAddress()
     {
-        var host = Dns.GetHostEntry(Dns.GetHostName());
-        foreach (var ip in host.AddressList)
+        try
         {
-            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            if (OperatingSystem.IsAndroid())
             {
-                return ip.ToString();
+                WifiManager wifiManager = (WifiManager)Android.App.Application.Context.GetSystemService(Service.WifiService);
+                int ipaddress = wifiManager.ConnectionInfo.IpAddress;
+                IPAddress ipAddr = new IPAddress(ipaddress);
+       
+                return ipAddr.ToString();
+            }
+            
+
+
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
             }
         }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            
+        }
+
         throw new Exception("No network adapters with an IPv4 address in the system!");
     }
-    
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
